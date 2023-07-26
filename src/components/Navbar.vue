26bba748ad3a53a3962efe5045023b7cb7082af4
Nilesh <nileshjadhav862001@gmail.com>
2023-07-26 22:16:16 +0530

    Pinia initial commit

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,7 +3,7 @@
     <nav class="navbar navbar-expand-lg navbar-light shadow" style="box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.1) !important">
         <i class="bi bi-list list"></i>
         <div class="container-fluid">
-            <a class="navbar-brand">Task Manager v1</a>
+            <a class="navbar-brand">Task Manager Pinia</a>
             <form class="d-flex" role="search">
                 <i style="padding: 10px; margin: 10px" class="bi bi-arrow-clockwise" @click="deletAllTask"></i>
                 <i style="padding: 10px; margin: 10px; cursor: pointer;" class="bi bi-plus-lg" @click="showAddTaskForm"></i>
